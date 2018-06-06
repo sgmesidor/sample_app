036f09ccb6e40d1e84a5e5362f1d1ece741548bf
Gabe <smesigabe@gmail.com>
2018-06-06 15:33:54 +0000

    Add hello action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  #adds "hello" action to the Application controller
+  def hello
+    render html:"The ships hung in the sky, much in the way bricks don't \nDouglas Adams"
+  end
+  
 end
